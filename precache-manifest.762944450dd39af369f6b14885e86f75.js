b7dcdfd42a08bcc6077122b84b17a99eeb27573e
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2019-08-21 12:44:19 +0300

    Updates

---
+++ precache-manifest.762944450dd39af369f6b14885e86f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d379990483c56ec0e569ad3833179ca",
+    "url": "/dataviz/index.html"
+  },
+  {
+    "revision": "3dec33af25ff27e2a85b",
+    "url": "/dataviz/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "e1b4c59805a7870f3e48",
+    "url": "/dataviz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3dec33af25ff27e2a85b",
+    "url": "/dataviz/static/js/2.0bd8f7cc.chunk.js"
+  },
+  {
+    "revision": "e1b4c59805a7870f3e48",
+    "url": "/dataviz/static/js/main.6963b1f8.chunk.js"
+  },
+  {
+    "revision": "4ac5500fc099aea8e32e",
+    "url": "/dataviz/static/js/runtime~main.cd602172.js"
+  }
+]);
